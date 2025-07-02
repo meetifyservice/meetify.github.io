b332c0d18ceab18d8409aef06fe58872fdf8580f
meetifyservice <meetify.service@gmail.com>
2025-07-02 14:46:18 +0200

    Add files via upload

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Dodajemy funkcjonalność klikania w logo
+    // Obsługa kliknięć na logo
     const logo = document.querySelector('.nav-logo');
     if (logo) {
         logo.addEventListener('click', () => {
             window.location.href = 'app.html';
+        });
+    }
+
+    // Obsługa kliknięć na kółeczko profilowe
+    const profileIcon = document.querySelector('.profile-icon');
+    if (profileIcon) {
+        profileIcon.addEventListener('click', () => {
+            const auth = firebase.auth();
+            if (auth.currentUser) {
+                window.location.href = `profile.html?id=${auth.currentUser.uid}`;
+            }
         });
     }
 });
