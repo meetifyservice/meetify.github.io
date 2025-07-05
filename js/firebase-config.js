88f916849da451b902ec13328b776e6698dbee4b
meetifyservice <meetify.service@gmail.com>
2025-07-06 01:48:35 +0200

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,8 +1,3 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import 'firebase/compat/storage';
-
 const firebaseConfig = {
   apiKey: "AIzaSyDbRBqhDdj05vXy8oDhpDrU9l2RT3Iz3xs",
   authDomain: "meetify-bf45c.firebaseapp.com",
@@ -12,11 +7,7 @@ const firebaseConfig = {
   appId: "1:824652857715:web:e4a4d0a0546490fcb58506"
 };
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
+firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
